402467b2195d8bbc05784be00fc7c3a059cdf5d2
ShubhamSamarth <shubhams@heaptrace.net>
2024-07-23 15:43:29 +0530

    Back to Injectable

--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -2,13 +2,13 @@ import { Component } from '@angular/core';
 
 import { NewTaskComponent } from './new-task/new-task.component';
 import { TasksListComponent } from './tasks-list/tasks-list.component';
-import { TasksService } from './tasks.service';
+// import { TasksService } from './tasks.service';
 
 @Component({
   selector: 'app-tasks',
   standalone: true,
   templateUrl: './tasks.component.html',
   imports: [NewTaskComponent, TasksListComponent],
-  providers: [TasksService], // Providing TasksService via the Element Injector. This service is only available within the TasksComponent and its child components, not globally throughout the application.
+  // providers: [TasksService], // Providing TasksService via the Element Injector. This service is only available within the TasksComponent and its child components, not globally throughout the application.
 })
 export class TasksComponent {}
