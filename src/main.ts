ae92a8e9cfdd4817147ef6fda1377c4b09f63dcb
ShubhamSamarth <shubhams@heaptrace.net>
2024-07-23 15:33:08 +0530

    Optimized way

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,11 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
-import { TasksService } from './app/tasks/tasks.service';
+// import { TasksService } from './app/tasks/tasks.service';
 
+/*
 bootstrapApplication(AppComponent, {
   providers: [TasksService],
 }).catch((err) => console.error(err)); // Note: Including TasksService here adds it to the bundle during application initialization. This might not be ideal if TasksService is not needed immediately when the application starts.
+*/
+
+bootstrapApplication(AppComponent).catch((err) => console.error(err)); // Bootstrap the application without TasksService in the initial bundle.
